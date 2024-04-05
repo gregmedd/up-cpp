4f11241ee3118c763eb17cf3aab0c971fa730c62
Greg Medding <gregory.medding@gm.com>
2024-04-05 14:43:37 -0700

    Improve constructor passthrough and ctor-time locking
    Supports passthrough of all constructor arguments supported by the
    underlying map type by forwarding variadic template arguments. Should
    support all current and future constructors for std::map and
    std::unordered_map.
    
    Improves handling of locks at constructor time by passing the 'other'
    object's mutex wrapped and locked in either std::shared_lock or
    std::unique_lock to the CopyMoveCtorLocker layer. This eliminates
    a) a private member variable that was only used at construction time
    and b) the need for a separate call to unlock() the mutex.

--- include/up-cpp/utils/SafeMap.h
+++ include/up-cpp/utils/SafeMap.h
@@ -46,28 +46,10 @@ template<class Key, class T,
     class Allocator = typename std::unordered_map<Key, T, Hash, KeyEqual>::allocator_type>
 using SafeUnorderedMap = BaseSafeMap<std::unordered_map<Key, T, Hash, KeyEqual, Allocator>>;
 
-/// Internal details for BaseSafeMap implementation
 namespace detail {
-/// @brief An ugly hack to lock at initialization time during copy and move construction
-///
-/// Used to lock the "other" object when copying/moving. Requires an explicit call
-/// to unlock() after the initializer list is done (in the ctor body).
-struct CopyMoveCtorLocker {
-    CopyMoveCtorLocker() = default;
-
-    enum class LockMode {
-        None, Unique, Shared
-    };
-
-    CopyMoveCtorLocker(LockMode mode, std::shared_mutex &m) noexcept;
-
-    LockMode unlock() noexcept;
-
-private:
-    LockMode cmclock_mode{LockMode::None};
-    std::unique_lock<std::shared_mutex> ulock;
-    std::shared_lock<std::shared_mutex> slock;
-};
+/// @remarks Inherits publicly from MapT
+template<typename MapT>
+struct CopyMoveCtorLocker;
 } // namespace detail
 
 /// @brief Wraps std::map in a std::shared_mutex for thread-safe access
@@ -76,8 +58,17 @@ private:
 /// the map. While there is still _some_ performance hit from adding the lock,
 /// this mitigates the worst of it.
 ///
-/// Aside from the locking, all interfaces are passthroughs to the underlying
-/// std::map class.
+/// Since locks must be held during access, a new transact() interface has been
+/// added. This allows for bulk operations, provided in the form of a callable,
+/// to be executed while the lock is held.
+///
+/// Aside from the locking and transactions, all interfaces are passthroughs
+/// to the underlying std::map class.
+///
+/// @tparam MapT Type of map container to be wrapped. Could be std::map or
+///              std::unordered_map, for example.
+///
+/// @note Inherits from MapT via detail::CopyMoveCtorLocker.
 ///
 /// @remarks We use protected inheritance from std::map so that any attempt to
 ///          use an interface that has not been protected by a mutex will fail
@@ -98,13 +89,14 @@ private:
 ///          (function pointer, lambda, etc) can be passed to transact(). The
 ///          lock will be held while the callable is running.
 template<typename MapT>
-class BaseSafeMap : private detail::CopyMoveCtorLocker, protected MapT {
+class BaseSafeMap : public detail::CopyMoveCtorLocker<MapT> {
     mutable std::shared_mutex map_lock_{};
-    using UnsafeMapT = MapT;
     using SafeMapT = BaseSafeMap<MapT>;
-    using CMCLock = detail::CopyMoveCtorLocker;
+    using CMCLock = detail::CopyMoveCtorLocker<MapT>;
 
 public:
+    using UnsafeMapT = MapT;
+
     ///////////////////////////////////////////////////////////////////////////
     /////////////// New interfaces for safe bulk transactions /////////////////
     ///////////////////////////////////////////////////////////////////////////
@@ -159,33 +151,54 @@ public:
     //using insert_return_type = typename MapT::insert_return_type;
     ////TODO Base-class-specific using statements (e.g. hasher)
 
-    // Pass through the base map class constructors to allow construction
-    // from an unsafe map. Also covers some of the generic iterator
-    // constructors.
-    using MapT::MapT;
+    ///////////////////////////////////////////////////////////////////////////
+    /// @name Constructors
+    /// Set of passthrough constructors, forwarding all arguments to the
+    /// underlying map type. In the case of copy or move construction from any
+    /// BaseSafeMap<T>, a lock object is added for CMCLock to hold during the
+    /// copy/move operation.
+    /// @{
 
-    // Define the constructors we need for copy and move
-    BaseSafeMap() = default;
+    /// @brief Default constructor
+    BaseSafeMap() : CMCLock() { }
 
-    BaseSafeMap(const SafeMapT &other)
-    : CMCLock(LockMode::Shared, other.map_lock_), MapT(other) {
-        CMCLock::unlock();
-    }
+    /// @brief Non-locking constructor taking some number of arguments
+    template<typename... Args>
+    BaseSafeMap(Args&&... args) : CMCLock(std::forward<Args>(args)...) { }
 
-    BaseSafeMap(const SafeMapT &other, const allocator_type &alloc)
-    : CMCLock(LockMode::Shared, other.map_lock_), MapT(other, alloc) {
-        CMCLock::unlock();
-    }
+    /// @brief Shared-mode locking copy constructor for BaseSafeMap<*>
+    /// @remarks Locks other.map_lock_ in shared mode for the duration of the
+    ///          copy operation.
+    template<typename OtherMapT>
+    BaseSafeMap(const BaseSafeMap<OtherMapT> &other)
+    : CMCLock(std::shared_lock(other.map_lock_), other) { }
 
-    BaseSafeMap(SafeMapT &&other)
-    : CMCLock(LockMode::Unique, other.map_lock_), MapT(std::move(other)) {
-        CMCLock::unlock();
-    }
+    /// @brief Shared-mode locking copy constructor for BaseSafeMap<*> taking
+    ///        some number of additional arguments.
+    /// @remarks Locks other.map_lock_ in shared mode for the duration of the
+    ///          copy operation.
+    template<typename OtherMapT, typename... Args>
+    BaseSafeMap(const BaseSafeMap<OtherMapT> &other, Args... args)
+    : CMCLock(std::shared_lock(other.map_lock_),
+            other, std::forward<Args>(args)...) { }
 
-    BaseSafeMap(SafeMapT &&other, const allocator_type &alloc)
-    : CMCLock(LockMode::Unique, other.map_lock_), MapT(std::move(other), alloc) {
-        CMCLock::unlock();
-    }
+    /// @brief Exclusive-mode locking move constructor for BaseSafeMap<*>
+    /// @remarks Locks other.map_lock_ in exclusive mode for the duration of
+    ///          the mov operation.
+    template<typename OtherMapT>
+    BaseSafeMap(const BaseSafeMap<OtherMapT> &&other)
+    : CMCLock(std::unique_lock(other.map_lock_), std::move(other)) { }
+
+    /// @brief Exclusive-mode locking move constructor for BaseSafeMap<*>
+    ///        taking some number of additional arguments.
+    /// @remarks Locks other.map_lock_ in exclusive mode for the duration of
+    ///          the move operation.
+    template<typename OtherMapT, typename... Args>
+    BaseSafeMap(const BaseSafeMap<OtherMapT> &&other, Args... args)
+    : CMCLock(std::unique_lock(other.map_lock_),
+            std::move(other), std::forward<Args>(args)...) { }
+    /// @}
+    ///////////////////////////////////////////////////////////////////////////
 
     ///////////////////////////////////////////////////////////////////////////
     /////////// Locking wrappers for existing std::map interfaces /////////////
@@ -314,29 +327,63 @@ public:
 // Should these be implemented with locking, or should we just dump that into
 // transactions?
 
+/// @brief Internal details for BaseSafeMap implementation
 namespace detail {
-CopyMoveCtorLocker::LockMode CopyMoveCtorLocker::unlock() noexcept {
-    if (cmclock_mode == LockMode::Unique) {
-        ulock.unlock();
-        ulock.release();
-    } else if (cmclock_mode == LockMode::Shared) {
-        slock.unlock();
-        slock.release();
-    }
-    auto original_mode = cmclock_mode;
-    cmclock_mode = LockMode::None;
-    return original_mode;
-}
+/// @brief Provides an interface for safely locking the 'other' object during
+///        copy and move construction.
+///
+/// When locking is required by BaseSafeMap's constructors, they can pass a
+/// locked std::unique_lock or std::shared_lock owning the mutex from the
+/// 'other' object. This lock will be held until MapT has been initialized,
+/// then checked to verify that the lock was owned throughout the process.
+///
+/// Otherwise, all parameters are forwarded to MapT's constructors, and public
+/// inheritance of MapT provides transparante access to all other public
+/// members.
+///
+/// @tparam MapT Type of map container to be wrapped. Could be std::map or
+///              std::unordered_map, for example.
+template<typename MapT>
+struct CopyMoveCtorLocker : protected MapT {
+    /// @brief Default constructor
+    CopyMoveCtorLocker() : MapT() { }
 
-CopyMoveCtorLocker::CopyMoveCtorLocker(LockMode mode, std::shared_mutex &m) noexcept
-: cmclock_mode(mode) {
-    if (cmclock_mode == LockMode::Unique) {
-        ulock = std::unique_lock(m);
-    } else if (cmclock_mode == LockMode::Shared) {
-        slock = std::shared_lock(m);
+    /// @brief Non-locking forwarding constructor
+    template<typename... Args>
+    CopyMoveCtorLocker(Args&&... args) : MapT(std::forward<Args>(args)...) { }
+
+    /// @brief Exclusive-mode locking / forwarding constructor
+    template<typename... Args>
+    CopyMoveCtorLocker(std::unique_lock<std::shared_mutex>&& l, Args&&... args)
+        : MapT(std::forward<Args>(args)...) {
+            check_lock(l);
+        }
+
+    /// @brief Shared-mode locking / forwarding constructor
+    template<typename... Args>
+    CopyMoveCtorLocker(std::shared_lock<std::shared_mutex>&& l, Args&&... args)
+        : MapT(std::forward<Args>(args)...) {
+            check_lock(l);
+        }
+
+private:
+    // @brief Checks that lock is not currently owned and throws an excception
+    // @throws std::invalid_argument if the lock is not currently owned
+    template<typename LockT>
+    static void check_lock(const LockT& l) {
+        if (!l.owns_lock()) {
+            throw std::invalid_argument(no_owned_lock_message);
+        }
     }
-}
+
+    /// @brief Message included in check_lock()'s thrown exception
+    static constexpr std::string_view no_owned_lock_message = 
+                "Attempted copy/move construction, but was unsuccessful in "
+                "taking ownership of other's mutex. Cannot guarantee "
+                "successful construction.";
+};
 } // namespace detail
+
 } // namespace uprotocol::utils
 
 #endif //UP_CPP_SAFE_MAP_H
