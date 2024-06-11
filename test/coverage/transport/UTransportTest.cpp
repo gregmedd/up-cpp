e9a2a70f049955cd593350dea1a32ce7e9f80e86
Greg Medding <gregory.medding@gm.com>
2024-06-11 12:12:44 -0700

    Pass UTransport's cleanup method for callbacks
    This method should be called when the `CalleeHandle` breaks the
    connection so that derived transports can clean up connections should
    they choose to do so. Enable this by passing a lambda that calls the
    method.

--- test/coverage/transport/UTransportTest.cpp
+++ test/coverage/transport/UTransportTest.cpp
@@ -14,7 +14,7 @@
 
 namespace {
 
-class TestFixture : public testing::Test {
+class UTransportTests : public testing::Test {
 protected:
 	// Run once per TEST_F.
 	// Used to set up clean environments per test.
@@ -23,8 +23,8 @@ protected:
 
 	// Run once per execution of the test application.
 	// Used for setup of all tests. Has access to this instance.
-	TestFixture() = default;
-	~TestFixture() = default;
+	UTransportTests() = default;
+	~UTransportTests() = default;
 
 	// Run once per execution of the test application.
 	// Used only for global setup outside of tests.
@@ -32,7 +32,63 @@ protected:
 	static void TearDownTestSuite() {}
 };
 
-// TODO replace
-TEST_F(TestFixture, SomeTestName) {}
+using namespace v1 = namespace uprotocol::v1;
+
+// TODO replace with UTransportMock
+class FakeUTransport : public uprotocol::transport::UTransport {
+	static v1::UStatus okStatus() {
+		v1::UStatus status;
+		status.set_code(v1::UCode::OK);
+		return status;
+	}
+
+	static v1::UUri someUri() {
+		v1::UUri uri;
+		uri.set_authority_name("SomeAuth");
+		uri.set_ue_id(1);
+		uri.set_ue_version_major(1);
+		uri.set_resource_id(0);
+	}
+
+	v1::UStatus sendImpl(const v1::UMessage& message) override {
+		return okStatus();
+	}
+
+	v1::UStatus registerListenerImpl(
+	    const v1::UUri& sink_filter, CallableConn&& listener,
+	    std::optional<v1::UUri>&& source_filter) override {
+		last_listener_ = std::move(listener);
+		return okStatus();
+	}
+
+	void cleanupListener(CallableConn listener) override {
+		last_cleaned_listener_ = listener;
+	}
+
+public:
+	FakeUTransport() : UTransport(someUri()) {}
+
+	size_t cleanup_count_{0};
+	CallableConn last_listener_;
+	CallableConn last_cleaned_listener_;
+}
+
+TEST_F(UTransportTests, CleanupGetsCalledWithListener) {
+	FakeUTransport transport;
+	auto handle = transport.registerCallback([]() {});
+
+	EXPECT_TRUE(handle);
+	EXPECT_TRUE(transport.last_listener_);
+	EXPECT_FALSE(transport.last_cleaned_listener_);
+	EXPECT_EQ(transport.cleanup_count_, 0);
+
+	handle.reset();
+
+	EXPECT_FALSE(handle);
+	EXPECT_FALSE(transport.last_listener_);
+	EXPECT_FALSE(transport.last_cleaned_listener_);
+	EXPECT_EQ(transport.cleanup_count_, 1);
+	EXPECT_EQ(transport.last_listener_, last_cleaned_listener_);
+}
 
 }  // namespace
